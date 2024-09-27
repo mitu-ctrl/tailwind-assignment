972005cb77de9d40927d6976aed77bf7383cfdf4
mitu-ctrl <moriomakter979@gmail.com>
2024-09-27 22:22:22 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+darkMode: 'class',
+
+  content: ["*"],
+  theme: {
+
+
+    extend: {
+
+      "fontFamily": {
+'jakarta':['Plus Jakarta Sans, sans-serif']
+
+      },
+
+      colors: {
+        'orange':'#F44335'
+        
+        }
+
+    },
+  },
+  plugins: [],
+}
+
